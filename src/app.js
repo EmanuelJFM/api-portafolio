b54df67b0b3d504b9dc07c852c70843d5c76c7ae
EmanuelJFM <emanuel.2211.noviembre@gmial.com>
2024-05-02 16:39:59 -0500

    feat: cors function

--- src/app.js
+++ src/app.js
@@ -1,7 +1,17 @@
 const express = require('express');
+const cors = require('cors'); // Importa el módulo cors
 const routes = require('./routes/routes');
 
 const app = express();
+const corsOptions = {
+    origin: 'https://portafolio-4b054.web.app',
+    optionsSuccessStatus: 200
+}
+
+app.use('/contact/send-email', cors(corsOptions), function(req, res, next){
+    console.log('CORS habilitado para /contact/send-email'); 
+    next(); 
+});
 
 app.use(express.json());
 app.use('/', routes);
